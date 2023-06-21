2d952e85c5136adeea1aa1c12cc7bbbb0acf76a8
Orçun Çolak <orcun.colak@hazelcast.com>
2023-06-21 10:44:05 +0300

    merged classes for client

--- src/main/java/com/colak/jet/jdbc/JdbcToIMapJob.java
+++ src/main/java/com/colak/jet/jdbc/JdbcToIMapJob.java
@@ -12,6 +12,8 @@ import com.hazelcast.map.IMap;
 
 import java.io.File;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcToIMapJob {
 
@@ -21,6 +23,7 @@ public class JdbcToIMapJob {
     private static final String MAP_NAME = "my-map";
 
     public static void jdbc(HazelcastInstance hazelcastInstance) {
+        destroyMap(hazelcastInstance);
 
 
         JetService jet = hazelcastInstance.getJet();
@@ -31,7 +34,7 @@ public class JdbcToIMapJob {
             File file = new File("mysql-connector-j-8.0.33.jar");
             URL jarResource = file.toURI().toURL();
             jobConfig.addJar(jarResource);
-            jobConfig.addClass(JdbcToIMapJob.class, Worker.class, ResultFunction.class);
+            jobConfig.addClass(JdbcToIMapJob.class);
 
             Pipeline pipeline = buildPipeline();
             Job job = jet.newJob(pipeline, jobConfig);
@@ -46,15 +49,52 @@ public class JdbcToIMapJob {
         }
     }
 
+    private static void destroyMap(HazelcastInstance hazelcastInstance) {
+        IMap<Object, Object> map = hazelcastInstance.getMap(MAP_NAME);
+        map.destroy();
+    }
+
+    public static Worker mapOutput(ResultSet resultSet) throws SQLException {
+        return new Worker(resultSet.getInt("id"), resultSet.getString("name"));
+    }
+
     private static Pipeline buildPipeline() {
         Pipeline pipeline = Pipeline.create();
         BatchSource<Worker> source = Sources.jdbc(URL,
                 QUERY,
-                ResultFunction::mapOutput);
+                resultSet -> new Worker(resultSet.getInt("id"), resultSet.getString("name")));
 
 
         pipeline.readFrom(source)
                 .writeTo(Sinks.map(MAP_NAME, Worker::getId, Worker::getName));
         return pipeline;
     }
+
+    public static class Worker {
+
+        int id;
+        String name;
+
+        public Worker(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
